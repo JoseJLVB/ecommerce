ede476d65992b1868a0961ed8b5d71dcc52dd725
Jose Luis Valcarcel Bravo <josevalcarcel@entrearaucarias.com>
2023-08-29 13:51:01 -0400

    React Ecommerce App

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+export function AppNavigation() {
+  return (
+    <View>
+      <Text>AppNavigation</Text>
+      <Text>AppNavigation</Text>
+      <Text>AppNavigation</Text>
+      <Text>AppNavigation</Text>
+      <Text>AppNavigation</Text>
+    </View>
+  );
+}
